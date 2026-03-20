94d4c426e42f3a5d7d847e9e1c2e37cf3220477b
Neil <neil.bottomley@blueyonder.co.uk>
2026-03-20 16:31:03 +0000

    Pipeline updates

--- src/simple_http_checker/checker.py
+++ src/simple_http_checker/checker.py
@@ -4,7 +4,6 @@ from typing import Collection
 
 logger = logging.getLogger(__name__)
 
-print(f"checker.py: {__name__}")
 
 def check_urls(urls: Collection[str], timeout: int = 5) -> dict[str, str]:
     """
@@ -15,7 +14,7 @@ def check_urls(urls: Collection[str], timeout: int = 5) -> dict[str, str]:
     """
 
     logger.info(f"Starting check for {len(urls)} URLs with a timeout of {timeout}")
-    results = {}
+    results: dict[str, str] = {}
 
     for url in urls:
         status = "UNKNOWN"
@@ -35,10 +34,14 @@ def check_urls(urls: Collection[str], timeout: int = 5) -> dict[str, str]:
             logger.warning(f"Connection error for {url}")
         except requests.exceptions.RequestException as e:
             status = f"REQUEST_ERROR: {type(e).__name__}"
-            logger.error(f"Unexpected request error occurred for {url}: {e}", exc_info=True)
+            logger.error(
+                f"Unexpected request error occurred for {url}: {e}", exc_info=True
+            )
 
         results[url] = status
-        logger.debug(f"Checked: {url:<40} -> {status}") #40 char width with left hand orientation
+        logger.debug(
+            f"Checked: {url:<40} -> {status}"
+        )  # 40 char width with left hand orientation
 
     logger.info("URL checks completed")
 
--- src/simple_http_checker/cli.py
+++ src/simple_http_checker/cli.py
@@ -5,22 +5,21 @@ from .checker import check_urls
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] %(levelname)-8s %(name)s %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S"
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 logger = logging.getLogger(__name__)
 
-print(f"cli.py: {__name__}")
 
 @click.command()
-@click.argument("urls", nargs=-1) #nargs == number of args with -1 == unlimited
+@click.argument("urls", nargs=-1)  # nargs == number of args with -1 == unlimited
 @click.option("--timeout", default=5, help="Timeout in seconds for each request.")
 @click.option("--verbose", "-v", is_flag=True, help="Enable debug logging")
-def main(urls, timeout, verbose):
+def main(urls: str, timeout: int, verbose: bool):
 
     if verbose:
         logger.debug("Verbose logging enabled")
-        logging.getLogger().setLevel(logging.DEBUG) #Set level from root logger
+        logging.getLogger().setLevel(logging.DEBUG)  # Set level from root logger
     if not urls:
         logger.warning("NO URLs provided.")
         click.echo("Usage: check-urls <URL1> <URL2> ....")
@@ -38,6 +37,5 @@ def main(urls, timeout, verbose):
         if "OK" in status:
             text_colour = "green"
         else:
-            text_colour="red"
+            text_colour = "red"
         click.secho(f"{url:<40} -> {status}", fg=text_colour)
-
